778110cfd5f5365afc67d9c8ac12d04a2f24d84f
KAAN <alikaanozoll@gmail.com>
2024-09-06 23:22:37 +0300

    Add project files.

---
+++ PatikaLibrary/BooksTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaLibrary
+{
+    public class BooksTable // Kitap nesnelerinin tanımlanması ||Identifying book objects
+    {
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorSurname { get; set; }       // Özellikler Bookstable sınıfına aittir. Bookstable nesnesinin verilerini saklar. || Properties belong to the Bookstable class. It stores the data of the Bookstable object.
+        public int NumberofPages { get; set; }
+        public string Publisher { get; set; }
+
+        public DateTime EntryTime;
+
+        public BooksTable()
+        { 
+            EntryTime  = DateTime.Now;
+
+        }         // Yapıcı Metotlar, nesneleri oluşturduğumuzda kullanılır.Nesnenin başlangıç değerini belirler. || Constructor Methods are used while creating objects. It determines the initial value of the object.
+        public BooksTable(string name,string authorname,string authorsurname, int numberofpages,string publisher)
+        {
+            Name = name;
+            AuthorName = authorname;
+            AuthorSurname = authorsurname;
+            NumberofPages = numberofpages;
+            Publisher = publisher;
+            EntryTime = DateTime.Now;
+
+        }
+
+        public void CreateBook()
+        {
+            Console.WriteLine($"Oluşturulan kitabımız {Name}, Kitabımızın yazarı {AuthorName} {AuthorSurname} ,Kitabımızın sayfa sayısı {NumberofPages} ve Yayınevimiz {Publisher} ");
+        }
+
+    }
+}
---
+++ PatikaLibrary/Program.cs
@@ -0,0 +1,19 @@
+﻿using PatikaLibrary;
+using System.Xml.Linq;
+
+BooksTable book = new BooksTable(); // BooksTable sınıfından yeni bir nesne oluşturuyoruz. || We create a new object from the BooksTable class.
+book.Name = "Aylin";
+book.AuthorName = "Ayşe";
+book.AuthorSurname = "Kulin";
+book.NumberofPages = 398;
+book.Publisher = "Remzi Kitabevi";
+book.CreateBook();
+
+Console.WriteLine("--------------------------");
+
+BooksTable book2 = new BooksTable("Sefiller","Victor","Hugo",1724,"Can Kitabevi");
+book2.CreateBook();
+
+
+
+
